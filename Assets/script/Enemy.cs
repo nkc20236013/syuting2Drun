df6adcea8558b12808a8cd427b17a162342aec84
nkc20236016 <nkc23.awane.minato@gmail.com>
2023-07-13 11:10:59 +0900

    0713追加

---
+++ Assets/script/Enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     //ƒRƒR‚É“G‚Å‚ ‚é‚Ë‚¸‚İ‚Ì‹““®‚ğ\¬‚·‚é
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
