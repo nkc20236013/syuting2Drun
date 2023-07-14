cbe10257bbbb0cfcc550b56bcdc386172fee6aa3
nkc20236016 <nkc23.awane.minato@gmail.com>
2023-07-14 09:55:15 +0900

    Revert "tuika"
    This reverts commit b515f839fac4aefe6670021d956ca28adf3c5962.

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
     void Start()
     {
      //ƒRƒR‚É“G‚Å‚ ‚é‚Ë‚¸‚İ‚Ì‹““®‚ğ\¬‚·‚é
