a3e4617e8bc2bdac3a02d8c53470263c0b28edf8
nkc20236016 <nkc23.awane.minato@gmail.com>
2023-07-18 14:07:30 +0900

    Revert "tuika"
    This reverts commit cce6c10c7afd230017cf04f8a73219b46cf05c01.

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -1,23 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal.Profiling.Memory.Experimental.FileFormat;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
     void Start()
     {
-        ////ココに敵であるねずみの挙動を構成する
-        //Destroy(gameObject, 6);		    // 寿命
-        //enemyType = Random.Range(0, 3); // 敵の種類
-        //speed = 5;                      // 移動速度
-        //dir = Vector3.left;             // 移動方向
-        //rad = Time.time;                // サインカーブの動きをずらす用
-        //shotTime = 0;                   // 弾発射間隔計算用
-
-        //// GameDirectorコンポーネントを保存
-        //gd = GameObject.Find("").GetComponent<GameDirector>();
+     //ココに敵であるねずみの挙動を構成する
 
     }
 
