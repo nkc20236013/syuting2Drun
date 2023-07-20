214c40aab1ca1a14f6009460292d076a17ebad6e
nkc20236002 <nkc23.kanamaru.ayumu@gmail.com>
2023-07-20 11:20:51 +0900

    7月20日更新

---
+++ Assets/Player/PleyerCon.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PleyerCon : MonoBehaviour
+{
+    Vector3 dir;
+    float speed;
+    Animator animator;
+
+
+    void Start()
+    {
+        //ÉRÉRÇ…playerÇ≈Ç†ÇÈîLÇÃìÆÇ´Çç\ê¨Ç∑ÇÈ
+        speed = 7;
+        this.animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        transform.position += dir.normalized * speed * Time.deltaTime;
+
+        float speedx = Mathf.Abs(dir.x);
+
+        // âÊñ ì‡êßå¿
+        Vector2 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -8.5f, 8.5f);
+        pos.y = Mathf.Clamp(pos.y, -5f, 5f);
+        transform.position = pos;
+
+        this.animator.speed = speedx / 1.5f;
+
+        if(dir.x!=0)
+        {
+            transform.localScale = new Vector3(dir.x, 5, 1);
+        }
+    }
+
+
+}
